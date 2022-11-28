0bc3db8ae1adbed7409dac15a25a5f0bab799bee
Alexandra Egorova <egorova.aleksandra@gmail.com>
2022-11-28 21:52:49 +0300

    changed Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,18 +18,42 @@ function Projects() {
     fetchProjects()
   }, []);
 
+  // return (
+  //   <div className="App">
+  //     {projectError &&
+  //       <div className="aboutPage">An error has occurred ${projectError}</div>
+  //     }
+  // {
+  //   isProjectsLoading
+  //     ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 250 }}><Loader /></div>
+  //     : <ProjectList projects={projects} title="Projects"/>}
+  //   </ div>
+  //   )  
+
+  
   return (
     <div className="App">
-      {projectError &&
-        <div className="aboutPage">An error has occurred ${projectError}</div>
-      }
-  {
-    isProjectsLoading
-      ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: 250 }}><Loader /></div>
-      : <ProjectList projects={projects} title="Projects"/>}
-    </ div>
-    )  
+      <div className="header">
+        <div className="pageTitle">Проекты</div>
+        <div className='header_of_tasks'>
+          <div className="id">№</div>
+          <div className='projectName'>Проекты</div>
+          <div className='description'>Описание</div>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='projects'>
+          {projects.map((project) => (
+            <div className='project'>
+              <div className='id'>{project.id}</div>
+              <div className='projectName'>{project.title}</div>
+              <div className='description'>{project.description}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 };
-
 
 export default Projects;
