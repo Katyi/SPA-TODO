e746b126096286661cd5ab8a8a40ce0189b09631
Alexandra Egorova <egorova.aleksandra@gmail.com>
2024-02-16 20:21:39 +0200

    changed App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./components/AppRouter";
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
 import './styles/App.css';
-// import { DndProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
   return (
-    // <DndProvider backend={HTML5Backend}>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    // </DndProvider>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
